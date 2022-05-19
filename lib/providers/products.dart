e3beb47aef11fec17006755205b786b21dc23c64
Amabel <amabel.levint98@gmail.com>
2022-05-19 17:54:37 +0700

    course 235

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -73,4 +73,9 @@ class Products with ChangeNotifier {
       notifyListeners();
     }
   }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((element) => element.id == id);
+    notifyListeners();
+  }
 }
